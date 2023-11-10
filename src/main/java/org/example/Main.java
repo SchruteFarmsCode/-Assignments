56be05c5d8ffe87676f0b610609d54c41148e0e1
SchruteFarmsCode <szymon.teperek99@o2.pl>
2023-11-10 13:59:15 +0100

    Part: 2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,8 @@
 package org.example;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Locale;
 
 public class Main {
     public static void main(String[] args) {
@@ -25,13 +27,13 @@ public class Main {
         String textToCheck = "Monticolo";
         try {
             validateLenght(textToCheck);
-            System.out.println(textToCheck + "--> Text is ok");
+            System.out.println("4. " + textToCheck + "--> Text is ok");
         } catch (TextTooLongException e) {
-            System.out.println(textToCheck + " --> text is too long");
+            System.out.println("4. " + textToCheck + " --> text is too long");
             e.getMessage();
 //            e.printStackTrace();
         } catch (TextTooShortException e) {
-            System.out.println(textToCheck + "  --> Text is too short");
+            System.out.println("4. " + textToCheck + "  --> Text is too short");
             e.getMessage();
 //            e.printStackTrace();
         }
@@ -39,27 +41,34 @@ public class Main {
         String myFiveExample = "abc";
         String result5 = reverse(myFiveExample);
         System.out.println("5.Orignal text: " + myFiveExample + ", Reversed text: " + result5);
-
-
-        //    6.FactorialLoop:
-        int factoriall = 4;
-        int result6 = factiorial(factoriall);
-        System.out.println("Factorial of number: " + factoriall + " == " + result6);
-//7.
-//        String word1 = "baton";
-//        String word2 = "noob";
-//        if(checkPalindrome(word1, word2)) {
-//            System.out.println("Both are Palindrome");
-//        }else {
-            System.out.println("One of them is a Palindrome");
+//        6.
+        String text1 = "arbuz";
+        String text2= "burza";
+        if (checkAnagram(text1, text2)) {
+            System.out.println("6.Texts: "  + text1 + " and " + text2 + " are Anagrams");
+        }else {
+            System.out.println("6.Texts: "  + text1 + " and " + text2 + " aren't  Anagrams");
         }
 
 
+//7.-metodę "checkPalindrome", która przyjmie dwa teksty i zwróci prawda lub fałsz w zależności czy oba wyrazy są swoimi palindromami
+        String word1 = "ara";
+        String word2 = "noob";
+        if (checkPalindrome(word1, word2)) {
+            System.out.println("7.Both are Palindrome");
+        } else {
+            System.out.println("7.One of them is a Palindrome || None of them are Palindromes");
 
+        }
+        //    8.metodę "factorial", która przyjmie liczbę i zwróci jej silnię:
+        int factoriall = 4;
+        int result6 = factiorial(factoriall);
+        System.out.println("8.Factorial(" + factoriall + ") = " + result6);
 
-
-
-
+//        9.metodę "fibonacci", która przyjmie liczbę n i zwróci n-tą liczbę ciągu liczb Fibonacciego
+        int n = 4;
+        System.out.println("9.F(" + n + ") = " + fibonacci(n));
+    }
 
 
     private static int divideByTwo(int num) {
@@ -82,42 +91,75 @@ public class Main {
             throw new TextTooShortException("Text is too short");
         }
     }
-    private  static String reverse(String example) {
+
+    private static String reverse(String example) {
         StringBuilder reversed = new StringBuilder(example);
         return reversed.reverse().toString();
+    }
+
+    private static boolean checkAnagram(String text1, String text2) {
+        text1 = text1.replaceAll("\\s","").toLowerCase();
+        text2 = text2.replaceAll("\\s", "").toLowerCase();
+        if (text1.length() != text2.length()) {
+            return false;
         }
-        private static boolean checkAnagram(boolean a) {
-        return a;
-        }
-//        private static boolean checkPalindrome(String text1, String text2) {
+        char[] charArray1 = text1.toCharArray();
+        char[] charArray2 = text2.toCharArray();
+        Arrays.sort(charArray1);
+        Arrays.sort(charArray2);
+        return Arrays.equals(charArray1, charArray2);
+    }
+
+    //        private static boolean checkPalindrome(String text1, String text2) {
 //        if (text1)
 //        return
 //        }
-//    private static boolean checkPalindrome(String text1, String text2) {
-//        String myText1 = text1.replaceAll("\\s", "").toLowerCase();
-//        String myText2 = text2.replaceAll("\\s", "").toLowerCase();
-//        String reversedText2 = new StringBuilder(myText2).reverse().toString();
-//        return myText1.equals(reversedText2);
-//    }
+    private static boolean checkPalindrome(String text1, String text2) {
+        String combined = text1 + text2;
+        int lenght = combined.length();
+        for (int i = 0; i < lenght / 2; i++) {
+            if (combined.charAt(i) != combined.charAt(lenght - 1 - i)) {
+                return false;
+            }
+        }
+        return true;
+
+    }
 
     private static int factiorial(int n) {
         int obj = 1;
         for (int i = 1; i <= n; i++) {
-            obj  *= i;
-            }
+            obj *= i;
+        }
         return obj;
+
+    }
+    private static int fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        int x1 = 0;
+        int x2 = 1;
+        for (int i = 2; i <= n; i++) {
+            int temp = x1 + x2;
+            x1 = x2;
+            x2 = temp;
+        }
+        return x2;
+
+    }
+    }
+
+    class TextTooLongException extends Exception {
+        public TextTooLongException(String text) {
+            super(text);
 
         }
     }
-        class TextTooLongException extends Exception {
-            public TextTooLongException(String text) {
-                super(text);
 
-            }
+    class TextTooShortException extends Exception {
+        public TextTooShortException(String text) {
+            super(text);
         }
-        class TextTooShortException extends Exception {
-            public TextTooShortException(String text) {
-                super(text);
-            }
-        }
+    }
 
